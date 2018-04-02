061b66d8f4760e4a282c281ebaadd3d08b059e31
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-04-01 22:16:20 -0700

    shredded down animation to simplist function. time to dazzle up transition animation

--- src/main.js
+++ src/main.js
@@ -34,10 +34,10 @@ class App extends React.Component {
               <li> Nobel Peace Prize</li>
             </ul>
           </h5>
-          <p className= "p-content" data-hover="JavaScript developer that has built full-stack web applications with server-side Node.js and modern, client-side, front-end libraries and frameworks, including React, implementing best industry practices.
+          <p className="container"><div className="p-hide">JavaScript developer that has built full-stack web applications with server-side Node.js and modern, client-side, front-end libraries and frameworks, including React, implementing best industry practices.
               My higher education focused on positive psychology, implementing positive practices into everyday activities. With a history of visual arts, screen printing, sketching, abstract painting, wardrobe design, manufacturing and various creative practices.
-            I intend on applying my critical thinking skills and positive practices attained from my post grad studies and exercise my creative ambitions to challenge the technical industry, and redefine the definition of innovation.">
-            <b><div>Yohanes Demissie: </div></b>
+            I intend on applying my critical thinking skills and positive practices attained from my post grad studies and exercise my creative ambitions to challenge the technical industry, and redefine the definition of innovation.</div>
+          <b><div className="p-show">Yohanes Demissie: </div></b>
           </p>
         </body>
         <footer>
