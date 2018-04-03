d9d6c525dacf98473e75b9165c1cc86cc4c4defd
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-04-02 22:15:49 -0700

    started adding on hover affects to hide/show content

--- src/main.js
+++ src/main.js
@@ -24,21 +24,13 @@ class App extends React.Component {
           <h3>
             An aspired innovator and revolutionist implementing critical thinking, team dynamic, creative solutions and full-stack applications; moving society forward and ultimately making our world a better place!
           </h3>
-          <h5> A Few Things Yohanes Aspires To Accomplish:
-            <ul>
-              <li>Create The Most Successful Operating System</li>
-              <li>Historic Recognition</li>
-              <li>All new innovative technology</li>
-              <li>Revolutionary Software</li>
-              <li>Accumulate $100,000,000+</li>
-              <li> Nobel Peace Prize</li>
-            </ul>
-          </h5>
-          <p className="container"><div className="p-hide">JavaScript developer that has built full-stack web applications with server-side Node.js and modern, client-side, front-end libraries and frameworks, including React, implementing best industry practices.
+          <p className="container-dev"><b><div className="p-show">What Type of Developer Am I? </div></b><div className="p-hide">JavaScript developer that has built full-stack web applications with server-side Node.js and modern, client-side, front-end libraries and frameworks, including React, implementing best industry practices.
               My higher education focused on positive psychology, implementing positive practices into everyday activities. With a history of visual arts, screen printing, sketching, abstract painting, wardrobe design, manufacturing and various creative practices.
             I intend on applying my critical thinking skills and positive practices attained from my post grad studies and exercise my creative ambitions to challenge the technical industry, and redefine the definition of innovation.</div>
-          <b><div className="p-show">Yohanes Demissie: </div></b>
           </p>
+          <p className="container-education"><b><div className="education-show">Education</div></b><div className="education-cf">Code Fellows</div><div className="cf-experience">At Code Fellows... we implemented modern practices... front-end design....back-end data base.... full-stac applicatons</div>
+          </p>
+          <p className="container-evergreen"><div className="evergreen-show">The Evergreen State College</div><div className="education-eg"></p>
         </body>
         <footer>
           Technical Tools
